cd67267b2d45fda8b0002d46c8c15a7b67c6f02f
ViniciusEmmanuel <vinicius.teixeira.m@gmail.com>
2019-03-25 01:45:50 -0300

    Correção do commit

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,13 +2,8 @@ const expres = require('express');
 const router = expres.Router();
 const watson = require('../api/watson');
 
-//Get 
-router.get('/', (req,res)=>{
-  res.status(200).send('Ok')
-});
-
 //Rotas do watason
-router.post('/translate', watson.postTranslate);
-router.post('/personality', watson.postPersonality);
+router.post('/api/translate', watson.postTranslate);
+router.post('/api/personality', watson.postPersonality);
 
 module.exports = router;
